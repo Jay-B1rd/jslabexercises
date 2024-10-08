d6556b83ab7972d83059819cb9131b34a7e6581b
Jay <ihearthuskiez@gmail.com>
2024-10-08 09:10:37 -0600

    sorry displaying actually didnt work. now it does though

--- prob2.js
+++ prob2.js
@@ -3,6 +3,11 @@ let bidArray = [5, 6, 12, 20];
 function showBids() {
     // Create a function that will add the array of bids to the element
     console.log(bidArray);
+    let outputString = "";
+    for (let i = 0; i < bidArray.length; i++) {
+        outputString += `<p>${bidArray[i]}</p>`;
+    }
+    document.getElementById("bidOutput").innerHTML = outputString;
 }
 
 function deleteBids() {
@@ -13,6 +18,11 @@ function addBid(inputID) {
     // Create a function that will take the text in an input box and add it to the array of bids
     let inputElement = document.getElementById(inputID);
     let bidInput = Number.parseFloat(inputElement.value);
+
+    if (!bidInput) {
+        alert("Please enter a number value.");
+        return;
+    }
 
     bidArray.push(bidInput);
 
